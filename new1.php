1b23c342b8dcf65cde684967ac4cbad3075d5c6a
lannakamilly <lannakamillyfresmota@gmail.com>
2025-04-24 13:53:12 -0300

    lannaa

---
+++ new1.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/news.css">
+</head>
+<body>
+    <section>
+    <div class="slider">
+        <div class="list">
+            <div class="item active">
+                <img src="img/girassol2.png">
+                <div class="content">
+                    <p>Aquarius</p>
+                    <h2>Mar de Girassóis</h2>
+                    <p>
+                        Instituto Abepoli vai catalogar as abelhas presentes no ‘Mar de Girassóis’, em São José.
+                    </p>
+                </div>
+            </div>
+            <div class="item">
+                <img src="./img/sesi-parceria.jpg">
+                <div class="content">
+                    <p>Instituto Abepoli</p>
+                    <h2>Parceria Ambiental </h2>
+                    <p>
+                        O instituto Abepoli visitou a escola SESI para iniciar uma parceria educativa sobre a importância das abelhas nativas. 
+                    </p>
+                </div>
+            </div>
+            <div class="item">
+                <img src="./img/11.jpg">
+                <div class="content">
+                    <p>Instituto Abepoli</p>
+                    <h2>Noticia</h2>
+                    <p>
+                     
+                    </p>
+                </div>
+            </div>
+            <div class="item">
+                <img src="./img/oii.jpg">
+                <div class="content">
+                    <p>Instituto Abepoli</p>
+                    <h2>Noticia</h2>
+                    <p>
+                       
+                    </p>
+                </div>
+            </div>
+            <div class="item">
+                <img src="img/b5.png">
+                <div class="content">
+                    <p>Instituto Abepoli</p>
+                    <h2>Noticia</h2>
+                    <p>
+                      
+                    </p>
+                </div>
+            </div>
+        </div>
+      
+        <div class="arrows">
+            <button id="prev"><</button>
+            <button id="next">></button>
+        </div>
+        <div class="thumbnail">
+            <div class="item active">
+                <img src="./img/girassol1.png">
+                <div class="content">
+                    Noticia
+                </div>
+            </div>
+            <div class="item">
+                <img src="./img/parceria2.png">
+                <div class="content">
+                    Noticia
+                </div>
+            </div>
+            <div class="item">
+                <img src="./img/news3.png">
+                <div class="content">
+                    Noticia
+                </div>
+            </div>
+            <div class="item">
+                <img src="./img/e.png">
+                <div class="content">
+                    Noticia
+                </div>
+            </div>
+            <div class="item">
+                <img src="img/a5.png">
+                <div class="content">
+                    Noticia
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="section__container choose__container" id="about">
+    <div class="choose__image">
+      <img src="assets/choose.jpg" alt="choose" />
+    </div>
+    <div class="choose__content">
+      <p class="section__subheader">Why Choose Us?</p>
+      <h2 class="section__header">Plan Your Trip <span>With Us</span></h2>
+      <ul class="choose__list">
+        <li>
+          <span><i class="ri-verified-badge-fill"></i></span>
+          <div>
+            <h4>Best Price Gurantee</h4>
+            <p>
+              We ensure you get the most competitive rates for your travel
+              plans, making your dream destinations affordable.
+            </p>
+          </div>
+        </li>
+        <li>
+          <span><i class="ri-calendar-fill"></i></span>
+          <div>
+            <h4>Booking Options</h4>
+            <p>
+              Experience the ease of flexible booking options tailored to suit
+              your schedule and preferences.
+            </p>
+          </div>
+        </li>
+        <li>
+          <span><i class="ri-road-map-fill"></i></span>
+          <div>
+            <h4>Revising Track Map</h4>
+            <p>
+              Navigate your adventure with our personalized track maps
+              designed for seamless and enjoyable travel experiences.
+            </p>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section>
+
+
+    <script src="./js/news.js"></script>
+</body>
+</html>
