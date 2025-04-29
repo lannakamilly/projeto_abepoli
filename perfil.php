222a9f6d7531ba7488d846489a52f5220f07fdb7
JuliaConconi <conconijulia@gmail.com>
2025-04-29 16:19:58 -0300

    página de pefil (novo)

--- perfil.php
+++ perfil.php
@@ -1,11 +1,27 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
   <meta charset="UTF-8">
   <title>Atualizar Perfil</title>
   <link rel="stylesheet" href="./css/perfil.css">
+  <link href="https://cdn.jsdelivr.net/npm/remixicon@3.2.0/fonts/remixicon.css" rel="stylesheet" />
 </head>
 <body>
+  <div class="container">
+    <h2>Perfil administrador</h2>
+
+    <div class="info">
+      <label for="arquivo" class="choose-photo">
+        <i class="ri-camera-line"></i>
+      </label>
+      <p class="photo-text">Escolher foto de perfil</p>
+    </div>
+
+    <!-- restante do formulário aqui -->
+  </div>
+
+  <!-- <body>
 <div class="topo">
   <a href="login.php" class="icone-voltar">&#8592</a>
   <h1 class="titulo">ATUALIZAR PERFIL</h1>
@@ -64,5 +80,6 @@
       </div>
     </form>
   </div>
-</body>
+</body> -->
+
 </html>
