ef647ec46b3e758acd7616f8823a8982c39630dc
flaviaglenda <flaviaglenda15@gmail.com>
2025-05-08 10:18:42 -0300

    ajuste nav color

--- produtoss.php
+++ produtoss.php
@@ -14,25 +14,25 @@
 </head>
 
 <body>
-<nav>
-      <div class="nav__header">
-        <div class="nav__logo">
-          <a href="#">
-            <img src="./img/logo1.jpg" alt="logo" />
-          </a>
+    <nav>
+        <div class="nav__header">
+            <div class="nav__logo">
+                <a href="#">
+                    <img src="./img/logo1.jpg" alt="logo" />
+                </a>
+            </div>
+            <div class="nav__menu__btn" id="menu-btn">
+                <i class="ri-menu-3-line"></i>
+            </div>
         </div>
-        <div class="nav__menu__btn" id="menu-btn">
-          <i class="ri-menu-3-line"></i>
-        </div>
-      </div>
-      <ul class="nav__links" id="nav-links">
-        <li><a href="./index.php">Início</a></li>
-        <li><a href="./produtoss.php">Produtos</a></li>
-        <li><a href="./sobre.php">Ações</a></li>
-        <li><a href="./doacoes.php">Doações</a></li>
-        <li><a href="./saibamais.php">Saiba Mais</a></li>
-        <li><a href="./contato.php">Contato</a></li>
-      </ul>
+        <ul class="nav__links" id="nav-links">
+            <li><a href="./index.php">Início</a></li>
+            <li><a href="./produtoss.php">Produtos</a></li>
+            <li><a href="./sobre.php">Ações</a></li>
+            <li><a href="./doacoes.php">Doações</a></li>
+            <li><a href="./saibamais.php">Saiba Mais</a></li>
+            <li><a href="./contato.php">Contato</a></li>
+        </ul>
     </nav>
 
     <section class="produtos-section">
@@ -73,7 +73,8 @@
                     <img src="./img/produto1.jpeg" alt="Produto">
                     <p class="nome-produto">Mel de abelhas nativas</p>
                     <p class="preco-produto">R$ 190,00</p>
-                    <a href="https://wa.me/5512988176722?text=Tenho%20interesse%20no%20produto" target="_blank" class="btn-comprar">
+                    <a href="https://wa.me/5512988176722?text=Tenho%20interesse%20no%20produto" target="_blank"
+                        class="btn-comprar">
                         <i class="fa fa-whatsapp"></i> Comprar
                     </a>
                 </div>
@@ -82,7 +83,8 @@
                     <img src="./img/produto2.jpeg" alt="Produto">
                     <p class="nome-produto">Extrato de própolis</p>
                     <p class="preco-produto">R$ 50,00</p>
-                    <a href="https://wa.me/5512988176722?text=Tenho%20interesse%20no%20produto" target="_blank" class="btn-comprar">
+                    <a href="https://wa.me/5512988176722?text=Tenho%20interesse%20no%20produto" target="_blank"
+                        class="btn-comprar">
                         <i class="fa fa-whatsapp"></i> Comprar
                     </a>
                 </div>
@@ -91,7 +93,8 @@
                     <img src="./img/produto3.jpeg" alt="Produto">
                     <p class="nome-produto">Cera de abelhas nativas</p>
                     <p class="preco-produto">R$ 50,00</p>
-                    <a href="https://wa.me/5512988176722?text=Tenho%20interesse%20no%20produto" target="_blank" class="btn-comprar">
+                    <a href="https://wa.me/5512988176722?text=Tenho%20interesse%20no%20produto" target="_blank"
+                        class="btn-comprar">
                         <i class="fa fa-whatsapp"></i> Comprar
                     </a>
                 </div>
@@ -100,7 +103,8 @@
                     <img src="./img/produto5.jpeg" alt="Produto">
                     <p class="nome-produto">Pólen apícola</p>
                     <p class="preco-produto">R$ 50,00</p>
-                    <a href="https://wa.me/5512988176722?text=Tenho%20interesse%20no%20produto" target="_blank" class="btn-comprar">
+                    <a href="https://wa.me/5512988176722?text=Tenho%20interesse%20no%20produto" target="_blank"
+                        class="btn-comprar">
                         <i class="fa fa-whatsapp"></i> Comprar
                     </a>
                 </div>
@@ -108,7 +112,8 @@
                     <img src="./img/produtoooooooooo8.jpeg" alt="Produto">
                     <p class="nome-produto">Casa de colmeia</p>
                     <p class="preco-produto">R$ 80,00</p>
-                    <a href="https://wa.me/5512988176722?text=Tenho%20interesse%20no%20produto" target="_blank" class="btn-comprar">
+                    <a href="https://wa.me/5512988176722?text=Tenho%20interesse%20no%20produto" target="_blank"
+                        class="btn-comprar">
                         <i class="fa fa-whatsapp"></i> Comprar
                     </a>
                 </div>
@@ -116,7 +121,8 @@
                     <img src="./img/produto6.jpeg" alt="Produto">
                     <p class="nome-produto">Favo de mel</p>
                     <p class="preco-produto">R$ 60,00</p>
-                    <a href="https://wa.me/5512988176722?text=Tenho%20interesse%20no%20produto" target="_blank" class="btn-comprar">
+                    <a href="https://wa.me/5512988176722?text=Tenho%20interesse%20no%20produto" target="_blank"
+                        class="btn-comprar">
                         <i class="fa fa-whatsapp"></i> Comprar
                     </a>
                 </div>
@@ -124,7 +130,8 @@
                     <img src="./img/produto7.jpeg" alt="Produto">
                     <p class="nome-produto">Sementes de girassol</p>
                     <p class="preco-produto">R$ 09,00</p>
-                    <a href="https://wa.me/5512988176722?text=Tenho%20interesse%20no%20produto" target="_blank" class="btn-comprar">
+                    <a href="https://wa.me/5512988176722?text=Tenho%20interesse%20no%20produto" target="_blank"
+                        class="btn-comprar">
                         <i class="fa fa-whatsapp"></i> Comprar
                     </a>
                 </div>
@@ -132,76 +139,80 @@
                     <img src="./img/produto9.jpg" alt="Produto">
                     <p class="nome-produto">Camisa conscientização</p>
                     <p class="preco-produto">R$ 55,00</p>
-                    <a href="https://wa.me/5512988176722?text=Tenho%20interesse%20no%20produto" target="_blank" class="btn-comprar">
+                    <a href="https://wa.me/5512988176722?text=Tenho%20interesse%20no%20produto" target="_blank"
+                        class="btn-comprar">
                         <i class="fa fa-whatsapp"></i> Comprar
                     </a>
                 </div>
 
-    <div class="paginacao">
-        <button class="pagina ativa">1</button>
-        <button class="pagina">2</button>
-        <button class="pagina">3</button>
-        <button class="pagina">></button>
-    </div>
+                <div class="paginacao">
+                    <button class="pagina ativa">1</button>
+                    <button class="pagina">2</button>
+                    <button class="pagina">3</button>
+                    <button class="pagina">></button>
+                </div>
 
-    <div class="wave-shape-divider">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,
+                <div class="wave-shape-divider">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,
         82.39-16.72,168.19-17.73,250.45-.39C823.78,31,
         906.67,72,985.66,92.83c70.05,18.48,
         146.53,26.09,214.34,3V0H0V27.35A600.21,
         600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
-        </svg>
-    </div>
-
-    <footer class="abepoli-footer">
-        <div class="footer-content">
-            <div class="footer-col logo-col">
-                <img src="img/logo1.jpg" alt="Instituto Abepoli" class="footer-logo">
-            </div>
-
-            <div class="footer-col contact-col">
-                <h4>Contato</h4>
-                <p><i class="fa fa-envelope"></i> abepoli@gmail.com</p>
-                <div class="social-icons">
-                    <p>
-                        <a href="https://www.facebook.com/profile.php?id=100076095320985" target="_blank" style="text-decoration: none; color: inherit;">
-                            <i class="fa fa-facebook"></i> Instituto Abepoli
-                        </a>
-                    </p>
-                    <p>
-                        <a href="https://www.instagram.com/abepoli/" target="_blank" style="text-decoration: none; color: inherit;">
-                            <i class="fa fa-instagram"></i> @abepoli
-                        </a>
-                    </p>
-                    <p>
-                        <a href="https://wa.me/5512988176722" target="_blank" style="text-decoration: none; color: inherit;">
-                            <i class="fa fa-whatsapp"></i> (12) 98817-6722
-                        </a>
-                    </p>
-                    <p>
-                        <a href="./login.php" style="text-decoration: none; color: inherit;">
-                             Realizar login
-                        </a>
-                        </p>
+                    </svg>
                 </div>
-            </div>
 
-            <div class="footer-col dev-col">
-                <h4>Site desenvolvido por</h4>
-                <p>Flávia Glenda Guimarães Carvalho</p>
-                <p>Júlia da Silva Conconi</p>
-                <p>Kauã Albuquerque de Almeida</p>
-                <p>Lanna Kamilly Fres Motta</p>
-                <p>Miguel Borges da Silva</p>
-            </div>
-        </div>
+                <footer class="abepoli-footer">
+                    <div class="footer-content">
+                        <div class="footer-col logo-col">
+                            <img src="img/logo1.jpg" alt="Instituto Abepoli" class="footer-logo">
+                        </div>
 
-        <div class="footer-bottom">
-            <p>© Todos os direitos reservados</p>
-        </div>
-    </footer>
-    <script src="./js/nav.js"></script>
+                        <div class="footer-col contact-col">
+                            <h4>Contato</h4>
+                            <p><i class="fa fa-envelope"></i> abepoli@gmail.com</p>
+                            <div class="social-icons">
+                                <p>
+                                    <a href="https://www.facebook.com/profile.php?id=100076095320985" target="_blank"
+                                        style="text-decoration: none; color: inherit;">
+                                        <i class="fa fa-facebook"></i> Instituto Abepoli
+                                    </a>
+                                </p>
+                                <p>
+                                    <a href="https://www.instagram.com/abepoli/" target="_blank"
+                                        style="text-decoration: none; color: inherit;">
+                                        <i class="fa fa-instagram"></i> @abepoli
+                                    </a>
+                                </p>
+                                <p>
+                                    <a href="https://wa.me/5512988176722" target="_blank"
+                                        style="text-decoration: none; color: inherit;">
+                                        <i class="fa fa-whatsapp"></i> (12) 98817-6722
+                                    </a>
+                                </p>
+                                <p>
+                                    <a href="./login.php" style="text-decoration: none; color: inherit;">
+                                        Realizar login
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="footer-col dev-col">
+                            <h4>Site desenvolvido por</h4>
+                            <p>Flávia Glenda Guimarães Carvalho</p>
+                            <p>Júlia da Silva Conconi</p>
+                            <p>Kauã Albuquerque de Almeida</p>
+                            <p>Lanna Kamilly Fres Motta</p>
+                            <p>Miguel Borges da Silva</p>
+                        </div>
+                    </div>
+
+                    <div class="footer-bottom">
+                        <p>© Todos os direitos reservados</p>
+                    </div>
+                </footer>
+                <script src="./js/nav.js"></script>
 </body>
 
 </html>
