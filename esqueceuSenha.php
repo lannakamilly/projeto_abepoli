434a452bb08dac12ea07bd1d7942bb4d28087883
flaviaglenda <flaviaglenda15@gmail.com>
2025-05-23 21:43:33 -0300

    login feito

--- esqueceuSenha.php
+++ esqueceuSenha.php
@@ -5,33 +5,36 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Login - Instituto Abepoli</title>
-    <link rel="stylesheet" href="./css/esqueceuSenha.css" />
+    <link rel="stylesheet" href="./css/login.css" />
     <link rel="stylesheet" href="./css/style.css" />
 </head>
 
 <body>
     <div class="tela-login">
-        <a href="./login.php" class="voltar">
+        <a href="./index.php" class="voltar">
             <img src="./img/botao_voltar.png" alt="Voltar">
         </a>
 
         <div class="conteudo">
+            <div class="lado-esquerdo">
+
+            </div>
+
             <div class="lado-direito">
                 <div class="card-login">
-                    <h1>Esqueceu sua senha?</h1>
-                    <hr class="linha-titulo">
+                    <h1 class="esqueceuSenhaTitulo">Esqueceu sua senha?</h1>
+                    <hr class="linha-titulo-esqueceu">
                     <form>
                         <label for="email">Email</label>
                         <input type="email" id="email" placeholder="Digite seu email" />
 
                         <label for="senha">Nova senha</label>
                         <input type="password" id="senha" placeholder="Digite sua nova senha" />
-
                         <label for="senha">Confirmar senha</label>
-                        <input type="password" id="senha" placeholder="Confirme sua senha" />
+                        <input type="password" id="senha" placeholder="Confirme sua nova senha" />
 
-                        <a href="./login.php" class="botao-entrar">CONFIRMAR</a>
 
+                        <a href="./perfil.php" class="botao-entrar">ENTRAR</a>
                     </form>
                 </div>
             </div>
